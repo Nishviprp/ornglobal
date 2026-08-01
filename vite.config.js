3dc9e90a402d12317cd170eb8a26bda8e689e682
nishviprp <nishviprp@gmail.com>
2026-07-31 20:40:34 -0400

    Configure Vite to build to docs folder for GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,9 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   base: '/ornglobal/',
+  build: {
+    outDir: 'docs',
+  },
   plugins: [react(), tailwindcss()],
   server: {
     port: 5173,
